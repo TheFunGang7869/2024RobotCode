03e71b39d034eea5ce6b3e86cb47b1f0dea70a85
o-fugi <o.fugikawa@gmail.com>
2024-02-26 13:24:35 -0500

    added analog potentiometer in subsystem

--- src/main/java/frc/robot/subsystems/ScissorLift.java
+++ src/main/java/frc/robot/subsystems/ScissorLift.java
@@ -8,23 +8,40 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import frc.robot.Constants.ScissorLiftConstants;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ScissorLift extends SubsystemBase {
 
-private WPI_VictorSPX scissorSPX;
+  private WPI_VictorSPX scissorSPX;
+  private final AnalogPotentiometer pot;
+
   public ScissorLift() {
 
     scissorSPX = new WPI_VictorSPX(ScissorLiftConstants.scissorLiftMotorID);
+    pot = new AnalogPotentiometer(2, 100, 0);
 
   }
-public void turnoff(){
-  scissorSPX.set(0.0);
-}
-public void cleave(){
-  scissorSPX.set(ScissorLiftConstants.scissorLiftUpPower);
-}
-public void dismantle(){
-  scissorSPX.set(ScissorLiftConstants.scissorLiftDownPower);
-}
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("Scissor lift position", getLiftPosition());
+  }
+
+  public double getLiftPosition() {
+    return pot.get();
+  }
+
+  public void turnoff() {
+    scissorSPX.set(0.0);
+  }
+
+  public void cleave() {
+    scissorSPX.set(ScissorLiftConstants.scissorLiftUpPower);
+  }
+
+  public void dismantle() {
+    scissorSPX.set(ScissorLiftConstants.scissorLiftDownPower);
+  }
 
 }
